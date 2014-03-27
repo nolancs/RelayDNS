f5dd5673912629a49fd85efe35755b0b072ef80b
nolancs <nolancs@users.noreply.github.com>
2014-03-27 10:57:23 -0700

    Update Request.h

--- Request.h
+++ Request.h
@@ -23,28 +23,28 @@ using namespace std;
 //## Class: Request
 //##
 //##  Desc: Represents a request sent to the server. Used to store data as the
-//##		request is processed through various stages.
+//##        request is processed through various stages.
 //##
 //################################################################################
 
 class Request
 {
 public:
-	Request()
-	: mClientPacketID(0),
-	  mOurPacketID(0)
-	{
-	}
-	virtual ~Request()
-	{
-	}
-
-	DNSPacket									mPacket;
-	struct sockaddr_in							mClientAddr;
-	unsigned short								mClientPacketID;
-	unsigned short								mOurPacketID;
-	string 										mDomainName;
-	chrono::high_resolution_clock::time_point	mForwardedTime;
+    Request()
+    : mClientPacketID(0),
+    mOurPacketID(0)
+    {
+    }
+    virtual ~Request()
+    {
+    }
+    
+    DNSPacket                                   mPacket;
+    struct sockaddr_in                          mClientAddr;
+    unsigned short                              mClientPacketID;
+    unsigned short                              mOurPacketID;
+    string                                      mDomainName;
+    chrono::high_resolution_clock::time_point   mForwardedTime;
 };
 
 #endif
