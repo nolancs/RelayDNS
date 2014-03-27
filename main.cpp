60f0f40777dff0b4d96d325a82cf6db7194e97a2
nolancs <nolancs@users.noreply.github.com>
2014-03-27 11:04:13 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,53 +5,53 @@
 //////////////////////////////////////////////////////////////////////////////////
 //
 // Usage:
-//	./simpleServerDNS [<listenPort> <remoteDNSAddr> <remoteDNSPort>]
+//    ./simpleServerDNS [<listenPort> <remoteDNSAddr> <remoteDNSPort>]
 //
-//	listenPort: is what our server listens on. (default: 53)
-//	remoteDNSAddr: Where to forward DNS requests (default: 8.8.8.8)
-//	remoteDNSPort: Where to forward DNS requests (default: 53)
+//    listenPort: is what our server listens on. (default: 53)
+//    remoteDNSAddr: Where to forward DNS requests (default: 8.8.8.8)
+//    remoteDNSPort: Where to forward DNS requests (default: 53)
 //
 //
 //////////////////////////////////////////////////////////////////////////////////
 //
 // Approach:
 //
-//	4 threads handle the server. Summary of the thread processing loops below.
+//    4 threads handle the server. Summary of the thread processing loops below.
 //
-//	Inbox thread:
-//		- Reads packets on port 53 (blocking) [Socket #1]
-//		- Adds them to the processing queue as a request object
-//	Processing thread:
-//		- Pops request objects off the processing queue
-//		- Decodes and verifies raw packet data in the request object
-//		- (Optionally) checks cache and responds with cache hit [Done.]
-//		- Replaces the packet ID with our own ID
-//		- Sends packet to remote DNS server [Socket #2]
-//		- Adds request to the outbox
+//    Inbox thread:
+//        - Reads packets on port 53 (blocking) [Socket #1]
+//        - Adds them to the processing queue as a request object
+//    Processing thread:
+//        - Pops request objects off the processing queue
+//        - Decodes and verifies raw packet data in the request object
+//        - (Optionally) checks cache and responds with cache hit [Done.]
+//        - Replaces the packet ID with our own ID
+//        - Sends packet to remote DNS server [Socket #2]
+//        - Adds request to the outbox
 // Outbox thread:
-//		- Reads packet responses from remote DNS server [Socket #2]
-//		- Matches remote DNS server response with request object in the outbox
-//		- Removes request object from the outbox
-//		- Restores original packet ID to the response packet
-//		- Checks timeout threshold before sending
-//		- Sends response packet to the original requestee [Socket #1]
+//        - Reads packet responses from remote DNS server [Socket #2]
+//        - Matches remote DNS server response with request object in the outbox
+//        - Removes request object from the outbox
+//        - Restores original packet ID to the response packet
+//        - Checks timeout threshold before sending
+//        - Sends response packet to the original requestee [Socket #1]
 // Maintainence thread:
-//		- Runs every X milliseconds
-//		- Actively culls timed out request objects from the outbox
+//        - Runs every X milliseconds
+//        - Actively culls timed out request objects from the outbox
 //
 //////////////////////////////////////////////////////////////////////////////////
 //
 // Notes:
 //
 // To gracefully shutdown this server:
-//		"kill -s TERM <pid>" or just hit Ctrl+C
+//        "kill -s TERM <pid>" or just hit Ctrl+C
 //
 // To test the server running port 2000:
-// 		dig @127.0.0.1 -p 2000 google.com
+//         dig @127.0.0.1 -p 2000 google.com
 //
 // To run this on 53
-//		sudo lsof -i :53 | grep LISTEN
-//		stop any server on 53 (if any), run this one.
+//        sudo lsof -i :53 | grep LISTEN
+//        stop any server on 53 (if any), run this one.
 //
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -91,35 +91,35 @@ int main(int argc, char * const argv[])
     //
     // Load and parse the tree data file.
     //
-	//const char*	arg1 = argc > 1 ? argv[1] : "";
-	//const char*	arg2 = argc > 2 ? argv[2] : "";
-	//const char*	arg3 = argc > 2 ? argv[2] : "";
-	int			rc;
-
-	cout << "\nStarting server...\n";
-	unsigned short listenPort = argc > 1 ? atoi(argv[1]) : 53;
-	const char *fwdTo = argc > 2 ? argv[2] : "8.8.8.8"; /* Google's public DNS server */
-	unsigned short fwdToPort = argc > 3 ? atoi(argv[3]) : 53; 
-	
-	try
-	{
-		Server dnsServer(listenPort, fwdTo, fwdToPort);
-		rc = dnsServer.RunServer();
-		if (rc)
-		{
-			ReportError("Failed to run server with options: %u, %s, %u",
-				listenPort, fwdTo, fwdToPort);
-			return -1;
-		}
-		cout << "Done.\n";
-	}
-	catch(...)
-	{
-		ReportError("Exiting, exception caught");
-		return -1;
-	}
-
-	return 0;
+    //const char*    arg1 = argc > 1 ? argv[1] : "";
+    //const char*    arg2 = argc > 2 ? argv[2] : "";
+    //const char*    arg3 = argc > 2 ? argv[2] : "";
+    int            rc;
+    
+    cout << "\nStarting server...\n";
+    unsigned short listenPort = argc > 1 ? atoi(argv[1]) : 53;
+    const char *fwdTo = argc > 2 ? argv[2] : "8.8.8.8"; /* Google's public DNS server */
+    unsigned short fwdToPort = argc > 3 ? atoi(argv[3]) : 53;
+    
+    try
+    {
+        Server dnsServer(listenPort, fwdTo, fwdToPort);
+        rc = dnsServer.RunServer();
+        if (rc)
+        {
+            ReportError("Failed to run server with options: %u, %s, %u",
+                        listenPort, fwdTo, fwdToPort);
+            return -1;
+        }
+        cout << "Done.\n";
+    }
+    catch(...)
+    {
+        ReportError("Exiting, exception caught");
+        return -1;
+    }
+    
+    return 0;
 }
 
 
