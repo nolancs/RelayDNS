477b9e78aec1a687bfacb9b40f63668390b07b15
nolancs <nolancs@users.noreply.github.com>
2014-03-27 10:49:55 -0700

    Update Error.h

--- Error.h
+++ Error.h
@@ -9,11 +9,11 @@
 #define ERROR_H
 
 void ReportErrorMain(
-	const char *inFile, int	inLine, const char *inFunc,
-	const char *inFormat, ...);
+                     const char *inFile, int	inLine, const char *inFunc,
+                     const char *inFormat, ...);
 
 #define ReportError(ARGS...) \
-		ReportErrorMain(__FILE__, __LINE__, __func__, ARGS)
+    ReportErrorMain(__FILE__, __LINE__, __func__, ARGS)
 
 
 #endif
